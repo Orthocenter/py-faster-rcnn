5db0e4ec89e36e49929b5222bf30a63facb352ae
Yuxin Chen <chenyuxin.mail@gmail.com>
2018-07-16 11:07:22 -0500

    change anchor shape and size

--- lib/rpn/anchor_target_layer.py
+++ lib/rpn/anchor_target_layer.py
@@ -25,7 +25,8 @@ class AnchorTargetLayer(caffe.Layer):
 
     def setup(self, bottom, top):
         layer_params = yaml.load(self.param_str_)
-        anchor_scales = layer_params.get('scales', (8, 16, 32))
+        #anchor_scales = layer_params.get('scales', (8, 16, 32))
+        anchor_scales = layer_params.get('scales', [2.6000, 3.3800, 4.3940, 5.7122, 7.4259, 9.6536, 12.5497, 16.3146, 21.2090])
         self._anchors = generate_anchors(scales=np.array(anchor_scales))
         self._num_anchors = self._anchors.shape[0]
         self._feat_stride = layer_params['feat_stride']
--- lib/rpn/proposal_layer.py
+++ lib/rpn/proposal_layer.py
@@ -26,7 +26,8 @@ class ProposalLayer(caffe.Layer):
         layer_params = yaml.load(self.param_str_)
 
         self._feat_stride = layer_params['feat_stride']
-        anchor_scales = layer_params.get('scales', (8, 16, 32))
+        #anchor_scales = layer_params.get('scales', (8, 16, 32))
+        anchor_scales = layer_params.get('scales', [2.6000, 3.3800, 4.3940, 5.7122, 7.4259, 9.6536, 12.5497, 16.3146, 21.2090])
         self._anchors = generate_anchors(scales=np.array(anchor_scales))
         self._num_anchors = self._anchors.shape[0]
 
